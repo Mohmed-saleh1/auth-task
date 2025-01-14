95ed80972cdbce96bbbaeeec7a4635ca2518727e
mohmed-saleh1 <mohamed.s.kamaal@gmail.com>
2025-01-15 00:02:16 +0200

    chore: import the auth module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { DatabaseModule } from './core/database/database.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuthModule],
   controllers: [AppController],
   providers: [],
 })
