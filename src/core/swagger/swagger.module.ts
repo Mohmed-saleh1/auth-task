8f1ee94465355d32869253b9dc3946cd07a07270
mohmed-saleh1 <mohamed.s.kamaal@gmail.com>
2025-01-14 12:40:30 +0200

    feat: implement the swagger setup

---
+++ src/core/swagger/swagger.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+
+@Module({})
+export class SwaggerDocumentationModule {
+  static setup(app: INestApplication): void {
+    const config = new DocumentBuilder()
+      .setTitle('API Documentation')
+      .setDescription('API endpoints for authentication and product management')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api-docs', app, document);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerDocumentationModule } from './core/swagger/swagger.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  SwaggerDocumentationModule.setup(app);
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
